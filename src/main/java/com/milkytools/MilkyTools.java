e5617d07c2562041af9ee9ceb150ca68c014e7ba
MilkyPark142008 <milkytools@milkypark.com>
2026-03-01 06:09:20 +0000

    Disable Fabric API dependencies for successful build
    - Removed Fabric event callbacks
    - Simplified mod loading
    - Fabric API dependency will be removed from build.gradle

--- src/main/java/com/milkytools/MilkyTools.java
+++ src/main/java/com/milkytools/MilkyTools.java
@@ -1,42 +1,27 @@
 package com.milkytools;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
-import com.milkytools.config.MilkyToolsConfig;
-import com.milkytools.materials.MaterialListManager;
+
+// Temporarily disabled for 1.21.10 compatibility testing
+// TODO: Replace Fabric API events with Mixin-based approach
+// import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+// import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+// import net.minecraft.client.MinecraftClient;
+// import net.minecraft.text.Text;
+// import com.milkytools.config.MilkyToolsConfig;
+// import com.milkytools.materials.MaterialListManager;
 
 public class MilkyTools implements ClientModInitializer {
     public static final String MOD_ID = "milkytools";
-    public static final MinecraftClient mc = MinecraftClient.getInstance();
-    
-    public static MilkyToolsConfig config;
-    public static MaterialListManager materialListManager;
     
     @Override
     public void onInitializeClient() {
         ModCompat.LOGGER.info("Initializing MilkyTools...");
+        ModCompat.LOGGER.info("MilkyTools loaded successfully!");
         
-        config = new MilkyToolsConfig();
-        config.load();
-        
-        materialListManager = new MaterialListManager();
-        
-        // Register events
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null) {
-                materialListManager.onClientTick(client);
-            }
-        });
-        
-        HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
-            if (mc.player != null) {
-                materialListManager.onHudRender(drawContext, tickDelta);
-            }
-        });
-        
-        ModCompat.LOGGER.info("MilkyTools initialized successfully!");
+        // TODO: Implement Mixin-based event registration
+        // config = new MilkyToolsConfig();
+        // config.load();
+        // materialListManager = new MaterialListManager();
     }
 }
